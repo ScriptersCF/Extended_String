b77dc46b91a86fd6f7ece87388f9f9ef93938f56
acodon <40075233+acodon@users.noreply.github.com>
2019-11-28 19:30:44 +0000

    refactored isalpha

--- funcs/isalpha.lua
+++ funcs/isalpha.lua
@@ -1,9 +1,5 @@
-return function(Text)
-	for i = 1, #Text do
-		local Byte = Text:sub(i, i):upper():byte()
-		if 65 > Byte or Byte > 90 then
-			return false
-		end
-	end
-	return true
+-- | isalpha |
+-- %A = non-alphabetical character
+return function(text)
+	return text:find("%A") == nil
 end
