3740d94aca60f10976996d83408c40ee7db04bd7
acodon <40075233+acodon@users.noreply.github.com>
2019-11-28 19:32:10 +0000

    refactored isalnum

--- funcs/isalnum.lua
+++ funcs/isalnum.lua
@@ -1,9 +1,5 @@
-return function(Text)
-	for i = 1, #Text do
-		local Byte = Text:sub(i, i):upper():byte()
-		if (65 > Byte or Byte > 90) and (48 > Byte or Byte > 57) then
-			return false
-		end
-	end
-	return true
+-- | isalnum |
+-- %W = non-alphanumeric character
+return function(text)
+	return text:find("%W") == nil
 end
