183ec2d65c9784c9bd99d9c15e67378fbe284c20
acodon <40075233+acodon@users.noreply.github.com>
2019-11-28 19:30:07 +0000

    refactored isdigit

--- funcs/isdigit.lua
+++ funcs/isdigit.lua
@@ -1,9 +1,5 @@
-return function(Text)
-	for i = 1, #Text do
-		local Byte = Text:sub(i, i):upper():byte()
-		if 48 > Byte or Byte > 57 then
-			return false
-		end
-	end
-	return true
+-- | isdigit |
+-- %D = non-digit character
+return function(text)
+	return text:find("%D") == nil
 end
