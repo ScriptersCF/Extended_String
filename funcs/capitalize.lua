bfa50d10bf7b47c1cc4c175939082c5d327cd105
acodon <40075233+acodon@users.noreply.github.com>
2019-11-28 19:33:18 +0000

    refactored capitalize

--- funcs/capitalize.lua
+++ funcs/capitalize.lua
@@ -1,13 +1,4 @@
-return function(Text)
-	local CappedString = {}
-	for i, v in pairs(Text:split(" ")) do
-		if i == 1 then
-			local Char1 = v:sub(1, 1):upper()
-			local Remainder = v:sub(2):lower()
-			CappedString[#CappedString + 1] = Char1 .. Remainder
-		else
-			CappedString[#CappedString + 1] = v:lower()
-		end
-	end
-	return table.concat(CappedString, " ")
+-- | capitalize | Makes the first character uppercase, and the rest lowercase
+return function(text)
+	return text:sub(1, 1):upper() .. text:sub(2):lower()
 end
