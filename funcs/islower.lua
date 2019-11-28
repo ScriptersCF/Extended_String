13c1a2233f3cd8588cf1664041b5e1fc247ca3c3
acodon <40075233+acodon@users.noreply.github.com>
2019-11-28 19:29:09 +0000

    refactored islower

--- funcs/islower.lua
+++ funcs/islower.lua
@@ -1,9 +1,5 @@
-return function(Text)
-	for i = 1, #Text do
-		local Char = Text:sub(i, i)
-		if Char:lower() ~= Char then
-			return false
-		end
-	end
-	return true
+-- | islower | 
+-- %u = uppercase character
+return function(text)
+	return text:find("%u") == nil
 end
